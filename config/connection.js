d57421836d1801a8da6709d0c8e86adc9e966158
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-07-08 22:29:47 +0530

    import moment module to format the timestamp

---
+++ config/connection.js
@@ -0,0 +1,1 @@
+const { connect, connection } = require("mongoose");
---
+++ models/Thought.js
@@ -0,0 +1,3 @@
+const { Schema, model, Types } = require("mongoose");
+// import moment module to format the timestamp
+const moment = require("moment");

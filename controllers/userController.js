8c14775c8675b123cee2f3eb73455d5c44767c59
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-07-09 08:39:17 +0530

    getting single user in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,4 +7,16 @@ module.exports = {
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
-  
+   //Get single user
+   getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .populate("thoughts")
+      .populate("friends")
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User find with that ID!" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },

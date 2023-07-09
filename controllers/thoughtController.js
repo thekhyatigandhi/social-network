fad456b8d0b5c2aeecdfb9ccad1fe7f864a3979c
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-07-09 08:27:01 +0530

    delete a thought in thoughtController

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -49,3 +49,23 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+
+    //delete a thought
+    deleteThought(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+          .then((thought) =>
+            !thought
+              ? res.status(404).json({ message: "No thought find with this ID!" })
+              : User.findOneAndUpdate(
+                  { thoughts: req.params.thoughtId },
+                  { $pull: { thoughts: req.params.thoughtId } },
+                  { new: true }
+                )
+          )
+          .then((user) =>
+            !user
+              ? res.status(404).json({ message: 'Thought deleted, but no user found'})
+              : res.json({ message: 'Thought successfully deleted' })
+          )
+          .catch((err) => res.status(500).json(err));
+      },

35e52ca794aafb64d25b8ece02551884a303dc65
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-07-09 08:38:28 +0530

    getting all users in userController

---
+++ controllers/userController.js
@@ -0,0 +1,10 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+  //Get all users
+  getUser(req, res) {
+    User.find({})
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  
